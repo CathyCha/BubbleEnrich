28abee33544b4361df966e665d8e934ac394636d
Yeon Joo Cha <cathy455503@gmail.com>
2019-10-03 14:44:02 -0400

    BubbleEnrich function

--- R/BubbleEnrich.R
+++ R/BubbleEnrich.R
@@ -18,8 +18,31 @@
 #' @export
 BubbleGSEA <- function(geneList) {
 
+#We define fold change greater than 2 as DEGs
+geneList <- names(geneList)[abs(geneList)>2]
 
+#load in the DisGeNET annotations for disease to gene
+disgeneAnnot <- load("./data/disgeneAnnot.rda")
+disease2gene = disgeneAnnot[, c("diseaseId", "geneId")]
+disease2name = disgeneAnnot[, c("diseaseId", "diseaseName")]
 
+#enrichment using clusterProfiler's enricher() function
+enrich = enricher(geneList, TERM2GENE = disease2gene, TERM2NAME = disease2name)
+
+#plot the enrichment results as a bubble plot using ggplot
+g <- ggplot(enrich[], aes(x=BgRatio, y=-log(p.adjust), size = Count, fill = -log(p.adjust))) +
+  geom_point(shape = 21, alpha=0.3) +
+  scale_size(range = c(.1, 17), name="Count") +
+  geom_text_repel(aes(x = BgRatio, y = -log(p.adjust), label = Description), size = 2, point.padding = NA, segment.color = NA) +
+  ggtitle("Disease Ontology Enrichment") +
+  scale_fill_continuous(low = "plum1", high = "purple4") +
+  theme_bw() +
+  theme(legend.position = "bottom", legend.direction = "horizontal",
+        axis.text.x = element_text(angle=45, hjust = 1)) +
+  labs(fill = "-log(adjusted pvalue)", size = "Set Size") +
+  geom_text_repel(aes(x = BgRatio, y = -log(p.adjust), label = strsplit((geneID), split="/")) , size = 2)
+
+g
 }
 
 
