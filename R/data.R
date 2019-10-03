0fc3e3a4dc0e84194d8f06620937234c618d6e9f
Yeon Joo Cha <cathy455503@gmail.com>
2019-10-03 15:02:32 -0400

    added data.R

---
+++ R/data.R
@@ -0,0 +1,28 @@
+#' Disease to Gene Annotations from DisGeNET
+#'
+#'
+#' @source http://www.disgenet.org/static/disgenet_ap1/files/downloads/all_gene_disease_pmid_associations.tsv.gz
+#'
+#' @format A dataframe with 1548061 rows and 15 columns
+#' \describe{
+#'  \item{geneID}{}
+#'  \item{geneSymbol}{}
+#'  \item{DSI}{}
+#'  \item{DPI}{}
+#'  \item{diseaseId}{}
+#'  \item{diseaseName}{}
+#'  \item{diseaseType}{}
+#'  \item{diseaseClass}{}
+#'  \item{diseaseSemanticType}{}
+#'  \item{score}{}
+#'  \item{EI}{}
+#'  \item{YearInitial}{}
+#'  \item{YearFinal}{}
+#'  \item{pmid}{}
+#'  \item{source}{}
+#' }
+#' @examples
+#' \dontrun{
+#'  disgeneAnnot
+#' }
+#' "disgeneAnnot"
