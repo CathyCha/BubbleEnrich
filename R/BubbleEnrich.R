6a948649efbba0f6d502c1a8f97a1ed64e5ba911
Yeon Joo Cha <cathy455503@gmail.com>
2019-10-04 13:02:25 -0400

    dependencies

--- R/BubbleEnrich.R
+++ R/BubbleEnrich.R
@@ -29,17 +29,17 @@ diseaseTOname = disgeneAnnot[, c("diseaseId", "diseaseName")]
 enrich = clusterProfiler::enricher(geneList, TERM2GENE = diseaseTOgene, TERM2NAME = diseaseTOname)
 
 #plot the enrichment results as a bubble plot using ggplot
-g <- ggplot2::ggplot(enrich[], aes(x = BgRatio, y = -log(p.adjust), size = Count, fill = -log(p.adjust))) +
+g <- ggplot2::ggplot(enrich[], ggplot2::aes(x = BgRatio, y = -log(p.adjust), size = Count, fill = -log(p.adjust))) +
   ggplot2::geom_point(shape = 21, alpha = 0.3) +
   ggplot2:scale_size(range = c(.1, 17), name = "Count") +
-  ggrepel::geom_text_repel(aes(x = BgRatio, y = -log(p.adjust), label = Description), size = 2, point.padding = NA, segment.color = NA) +
+  ggrepel::geom_text_repel(ggplot2::aes(x = BgRatio, y = -log(p.adjust), label = Description), size = 2, point.padding = NA, segment.color = NA) +
   ggplot2::ggtitle("Disease Ontology Enrichment") +
   ggplot2::scale_fill_continuous(low = "plum1", high = "purple4") +
   ggplot2::theme_bw() +
   ggplot2::theme(legend.position = "bottom", legend.direction = "horizontal",
         axis.text.x = element_text(angle=45, hjust = 1)) +
   ggplot2::labs(fill = "-log(adjusted pvalue)", size = "Set Size") +
-  ggrepel::geom_text_repel(aes(x = BgRatio, y = -log(p.adjust), label = strsplit((geneID), split="/")) , size = 2)
+  ggrepel::geom_text_repel(ggplot2::aes(x = BgRatio, y = -log(p.adjust), label = strsplit((geneID), split="/")) , size = 2)
 
 g
 }
