408f7009900e46bfb41558cb32a2275b02c252d6
Yeon Joo Cha <cathy455503@gmail.com>
2019-10-04 13:58:20 -0400

    BubbleEnrich unittest file

---
+++ tests/testthat/test-BubbleEnrich.R
@@ -0,0 +1,9 @@
+context("Bubble GSEA")
+library(BubbleGSEA)
+library("testthat")
+
+test_that("the function output is a ggplot", {
+  # data(geneList, package = "DOSE")
+  # bubble_graph <- BubbleEnrich(geneList)
+  # expect_is(bubble_graph,"ggplot")
+})
